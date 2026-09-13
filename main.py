cf9c40db69899b1d0de401861c4c2b14140186b7
ragnar07-bit <programmazione.moderna@gmail.com>
2026-09-13 15:57:53 +0200

    Creazione dei file per l'accesso del giocatore e riorganizzazione file

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import lib.g2d as g2d
 import pygame
 from pathlib import Path
 from src.Giocatore  import Giocatore, CELL
-from Menu.Main_Menu  import MainMenu
+from Menu.Grafica.Main_Menu  import MainMenu
 
 # ===========================================================================
 # COSTANTI
